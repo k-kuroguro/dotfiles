a25656302f121e7ade72037c878ccbc9d4da3ab2
k-kuroguro <lingjiujinshan@gmail.com>
2025-04-12 00:49:43 +0900

    w

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ cd $(dirname ${BASH_SOURCE:-$0})
 mkdir -p ~/.bash_completion.d
 curl -fSsL "https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/master/completions/tmux" > ~/.bash_completion.d/tmux
 
-# TODO: fzf, cargo, eza, bat, gh, fs, tmux, dotman, uv
+# TODO: fzf, cargo, eza, bat, gh, fd, tmux, dotman, uv, HackGen NF, alacritty 
