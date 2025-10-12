71423d82c3d719bbef97b77cfe23bcdbb7598860
k-kuroguro <68765710+k-kuroguro@users.noreply.github.com>
2025-10-12 12:05:35 +0900

    w

--- config/zsh/sync/prompt.zsh
+++ config/zsh/sync/prompt.zsh
@@ -1,9 +1,9 @@
-prompt_command_timer_preexec() {
+__prompt_command_timer_preexec() {
    prompt_timer_start_ms=$(( $(printf "%.0f" "${EPOCHREALTIME} * 1000") ))
    prompt_elapsed_time=''
 }
 
-prompt_command_timer_precmd() {
+__prompt_command_timer_precmd() {
    if [[ -n "${prompt_timer_start_ms}" ]]; then
       local __timer_end_ms=$(( $(printf "%.0f" "${EPOCHREALTIME} * 1000") ))
       local elapsed_ms=$(( __timer_end_ms - prompt_timer_start_ms ))
@@ -39,7 +39,18 @@ ZSH_THEME_GIT_PROMPT_STASHED='$'
 ZSH_THEME_GIT_PROMPT_CLEAN=''
 ZSH_GIT_PROMPT_SHOW_STASH=1
 
-PROMPT='%F{10}%n%f'"${prompt_host}"':%F{14}%~%f$(gitprompt)${prompt_elapsed_time}'$'\n''❯ '
+# Display git prompt, removing the separator if no git status symbols are present.
+__prompt_git() {
+   local raw="$(gitprompt)"
+   local symbols="${ZSH_THEME_GIT_PROMPT_UNMERGED}${ZSH_THEME_GIT_PROMPT_STAGED}${ZSH_THEME_GIT_PROMPT_UNSTAGED}${ZSH_THEME_GIT_PROMPT_UNTRACKED}${ZSH_THEME_GIT_PROMPT_STASHED}"
 
-add-zsh-hook preexec prompt_command_timer_preexec
-add-zsh-hook precmd prompt_command_timer_precmd
+   if [[ "${raw}" != *["$symbols"]* ]]; then
+      raw="${raw/${ZSH_THEME_GIT_PROMPT_SEPARATOR}/}"
+   fi
+   echo -n "${raw}"
+}
+
+PROMPT='%F{10}%n%f'"${prompt_host}"':%F{14}%~%f$(__prompt_git)${prompt_elapsed_time}'$'\n''❯ '
+
+add-zsh-hook preexec __prompt_command_timer_preexec
+add-zsh-hook precmd __prompt_command_timer_precmd
