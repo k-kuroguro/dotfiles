538b6ad359bbb39015805e7e31edce83a81a596c
k-kuroguro <68765710+k-kuroguro@users.noreply.github.com>
2026-06-24 20:36:27 +0900

    Update

--- config/nvim/lua/configs/autocmd.lua
+++ config/nvim/lua/configs/autocmd.lua
@@ -1,3 +1,6 @@
 vim.api.nvim_create_autocmd("TermOpen", {
-   callback = function() vim.opt.statuscolumn = " " end,
+   callback = function(args)
+      local win = vim.fn.bufwinid(args.buf)
+      if win ~= -1 then vim.wo[win].statuscolumn = " " end
+   end,
 })
--- config/nvim/lua/configs/options.lua
+++ config/nvim/lua/configs/options.lua
@@ -1,10 +1,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.o.laststatus = 3
-
+vim.opt.laststatus = 3
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.signcolumn = "yes"
-vim.opt.statuscolumn = "%=%{v:relnum?v:relnum:v:lnum}%s"
-vim.opt.numberwidth = 2
